9afe3813153d323324348eb1116170d4b8b54c30
Rohith <113355984+RohithTasks@users.noreply.github.com>
2024-02-05 11:02:55 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,24 @@
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+    var randomImageSource1 = "images/" + randomDiceImage1;
+    document.getElementById("dice1").setAttribute("src", randomImageSource1);
+  
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+    var randomImageSource2 = "images/" + randomDiceImage2;
+    document.getElementById("dice2").setAttribute("src", randomImageSource2);
+  
+    // Determine the winner
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+      document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    } else {
+      document.querySelector("h1").innerHTML = "Draw!";
+    }
+  }
+  
+  // Add event listener to the button
+  document.getElementById("rollButton").addEventListener("click", rollDice);
+  
